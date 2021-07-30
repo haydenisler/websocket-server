fbf725ab037062378bb566142e5e130d036ecc68
Hayden Isler <me@haydenisler.com>
2021-07-29 23:28:47 -0500

    basic server/client comms
    Co-authored-by: jtessensohn <jtessensohn@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
+// const formatMessage = require('./helpers/formatDate')
+
+io.on('connection', (socket) => { 
+    socket.on('joinRoom', () => {
+        console.log('User has joined the room!')
+
+        socket.emit('message', { 
+            message: 'Hello from the server!'
+        });
+    });
+ });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+server.listen(3000);
