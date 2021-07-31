eae11927fa63913969d7194ef887e4b56b9bf296
Hayden Isler <me@haydenisler.com>
2021-07-30 23:48:16 -0500

    resolved cors on client

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require('express')();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST"]
+      }
+});
 const path = require('path');
 // const formatMessage = require('./helpers/formatDate')
 
